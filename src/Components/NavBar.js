6023addaa2873e4d3de17b13d6574d0af47ab496
Guido Lanteri <guidolanteri@hotmail.com>
2023-03-08 00:15:22 -0300

    PreEntrega1+Lanteri

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,16 @@
+import './NavBar.css'
+
 const NavBar = () => {
     return (
-        <nav>
+        <nav className= "NavBar" >
             <h1>Refrigeración Cormay</h1>
+            <div>
+                <button> Manifold </button>
+                <button> Pestañadora </button>
+                <button> Cortadora </button>
+            </div>
         </nav>
     )
 }
+
+export default NavBar
